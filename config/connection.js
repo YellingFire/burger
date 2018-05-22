5682f01ae6a78c4154d34c78f11bbb38bd95468d
Chad33 <chad.javarush@gmail.com>
2018-05-21 18:42:14 -0600

    JawsDB addition

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,24 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.NODE_ENV === "production") {
+  dbConnection = {
+    port: 3306,
+    host: "	s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "	tcrct2q8fv72ufiv",
+    password: "daizswdhpg6td5mj",
+    database: "gc2wo2xb475c9m3j"
+  }
+} else {
+  dbConnection = {
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  }
+}
+var connection = mysql.createConnection(dbConnection);
 
 // Make connection.
 connection.connect(function(err) {
